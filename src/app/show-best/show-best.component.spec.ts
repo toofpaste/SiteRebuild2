16c63de192ebeb0cff1a8dac48fe33fcff5f1643
Michael Larragueta <m.larragueta@yahoo.com>
2019-06-14 13:32:35 -0700

    Add component

---
+++ src/app/show-best/show-best.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowBestComponent } from './show-best.component';
+
+describe('ShowBestComponent', () => {
+  let component: ShowBestComponent;
+  let fixture: ComponentFixture<ShowBestComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShowBestComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowBestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/show-best/show-best.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-best',
+  templateUrl: './show-best.component.html',
+  styleUrls: ['./show-best.component.css']
+})
+export class ShowBestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
