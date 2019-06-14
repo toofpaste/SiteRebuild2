b2f84863f1c037db621088ab17c76a76749615a3
Michael Larragueta <m.larragueta@yahoo.com>
2019-06-14 13:16:59 -0700

    add objects for posts

---
+++ src/app/models/posts.model.ts
@@ -0,0 +1,4 @@
+export class Posts{
+  // tslint:disable-next-line:max-line-length
+  constructor(public title: string, public time: number, public username: string, public commentNum: number, public comments: string[], public upvotes: number, public link: string, public thumbnail: string) {};
+}
